731c2633f44d00067cca13117b659a7a20e8a57d
Greg West <gwest33@yahoo.com>
2018-08-10 15:40:25 -0400

    Add IsConnected var to comment

--- XamarinProject/XamarinProject.iOS/Main.cs
+++ XamarinProject/XamarinProject.iOS/Main.cs
@@ -16,7 +16,7 @@ namespace XamarinProject.iOS
     public class Application
     {
         // This is the main entry point of the application.
-        // Add Static var 
+        // Add Public Static var IsConnected
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
